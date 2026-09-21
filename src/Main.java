e3ef765369c6c63581aea5e2db4f052dc362cdd5
Marta Zygmunt <marta.zygmunt@yh.nackademin.se>
2026-09-21 13:23:05 +0200

    complete Nivå1 and nivå2 solution

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,47 @@
 
 public class Main {
-public static void main(String[] args) {
-    // Lampa
 
-    int lightId = 1;
-    boolean isLightOn = false;
-    int brightness = 0;
-    String doorZone = "Entré";
-    boolean isOpen = true;
+    public static void main(String[] args) {
+    // Nivå1
+        int lightId = 1;
+        boolean isLightOn = false;
+        int brightness = 0;
+        String doorZone = "Entré";
+        boolean isOpen = true;
 
-            if (isOpen) {
+        if (isOpen) {
         isLightOn = true;
         brightness = 80;
         System.out.println("Door " + doorZone + " are open!");
         System.out.println("Lamp status (" + isLightOn + ") is ON");
-
-            }
-            if (brightness >= 0 && brightness <= 100){
-
-            System.out.println("Brightness is valid");
-            } else {
-            System.out.println("Invalid brightness valid!");
         }
 
+        if (brightness >= 0 && brightness <= 100) {
+
+        System.out.println("Brightness is valid");
+        } else {
+        System.out.println("Invalid brightness valid!");
+        }
+
+        //Nivå 2
+
+        // Dimmerloop
+        for (int b = 0; b <= 100; b += 20) {
+        brightness = b;
+        System.out.println("Brightness: " + brightness + "%");
+        }
+
+        // 2. Toggle
+        isLightOn = !isLightOn;
+        System.out.println("Lamp status after toggle: " + isLightOn);
+
+
+        // 3. Felhantering
+        brightness = 120; //
+        if (brightness >= 0 && brightness <= 100) {
+        System.out.println("Brightness is valid");
+        } else {
+        System.out.println("Error: " + brightness + "% is invalid!");
+        }
     }
 }
